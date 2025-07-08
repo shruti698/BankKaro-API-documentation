28b439e402c49c770f93ab0665c33150e5cdf830
Mohsin <mohsinfd@gmail.com>
2025-07-08 12:07:59 +0530

    Restore dependencies and Vite config for Vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'es2015',
-    sourcemap: false,
-    minify: 'esbuild',
-    rollupOptions: {
-      external: ['clsx', 'react-is', 'react-transition-group', 'cookie', 'set-cookie-parser'],
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          mui: ['@mui/material', '@emotion/react', '@emotion/styled']
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    exclude: ['@mui/icons-material'],
-    include: ['@mui/material', '@emotion/react', '@emotion/styled']
-  }
+  plugins: [react()]
 })
